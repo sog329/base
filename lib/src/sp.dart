656217ef1c621a2d7d2ff4cd07a1cfdfd00b4bf7
sog329 <songxg0329@163.com>
2023-08-03 00:11:10 +0800

    udpate sp logic

--- lib/src/sp.dart
+++ lib/src/sp.dart
@@ -31,36 +31,33 @@ class Sp<V> {
 
   static Future<bool> _set<T>(String k, T v) {
     late Future<bool> fail = Future.value(false);
-    switch (T.runtimeType) {
-      case int:
-        return _sp.setInt(k, v as int);
-      case double:
-        return _sp.setDouble(k, v as double);
-      case bool:
-        return _sp.setBool(k, v as bool);
-      case String:
-        String s = v as String;
-        double kb = HpStr.kb(s);
-        if (kb <= 10) {
-          return _sp.setString(k, s);
-        } else {
-          HpDevice.log('Sp.set($k): String.kb=$kb, too large to save');
+    if (v is int) {
+      return _sp.setInt(k, v);
+    } else if (v is double) {
+      return _sp.setDouble(k, v);
+    } else if (v is bool) {
+      return _sp.setBool(k, v);
+    } else if (v is String) {
+      double kb = HpStr.kb(v);
+      if (kb <= 10) {
+        return _sp.setString(k, v);
+      } else {
+        HpDevice.log('Sp.set($k): String.kb=$kb, too large to save');
+        return fail;
+      }
+    } else if (v is List<String>) {
+      double kb = 0;
+      for (String s in v) {
+        kb += HpStr.kb(s);
+        if (kb > 10) {
+          HpDevice.log('Sp.set($k): List<String>.kb=$kb, too large to save');
           return fail;
         }
-      case const (List<String>):
-        List<String> lst = v as List<String>;
-        double kb = 0;
-        for (String s in lst) {
-          kb += HpStr.kb(s);
-          if (kb > 10) {
-            HpDevice.log('Sp.set($k): List<String>.kb=$kb, too large to save');
-            return fail;
-          }
-        }
-        return _sp.setStringList(k, lst);
-      default:
-        HpDevice.log('Sp.set($k): unknownType=${T.runtimeType}');
-        return fail;
+      }
+      return _sp.setStringList(k, v);
+    } else {
+      HpDevice.log('Sp.set($k): unknownType=${T.runtimeType}');
+      return fail;
     }
   }
 
